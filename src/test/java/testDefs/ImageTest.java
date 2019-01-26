274545872839ab24eead88ca93ed96cc24f97a2f
Arsens <arsens.morins@chi.lv>
2019-01-26 11:50:25 +0200

    sik2

---
+++ src/test/java/testDefs/ImageTest.java
@@ -0,0 +1,22 @@
+package testDefs;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class ImageTest {
+
+    @When("^I navigate to google homepage$")
+    public void iNavigateToGoogleHomepage() throws Throwable {
+
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^I verify that main image matches test image$")
+    public void iVerifyThatMainImageMatchesTestImage() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+}
+
